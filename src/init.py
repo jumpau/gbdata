2864d70440c6a1e739353716b9c739a01a61e88d
jumpau <jumpaulive@outlook.com>
2025-08-07 18:11:01 +0800

    Create init.py

---
+++ src/init.py
@@ -0,0 +1,5 @@
+"""Telegram Backup Tool with Permanent Links"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__description__ = "A tool to backup Telegram channels using permanent links instead of downloading files"
